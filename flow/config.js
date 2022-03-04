2423527fc92365af1b5c58a669359efb3b9a9e65
Ousikai <mtfallsVR@protonmail.com>
2022-03-03 19:41:41 -0500

    [200~Querying the Blockchain

--- flow/config.js
+++ flow/config.js
@@ -2,5 +2,6 @@ import { config } from "@onflow/fcl";
 
 config({
   "accessNode.api": "https://access-testnet.onflow.org", // Mainnet: "https://access-mainnet-beta.onflow.org"
-  "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn" // Mainnet: "https://fcl-discovery.onflow.org/authn"
+  "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn", // Mainnet: "https://fcl-discovery.onflow.org/authn"
+  "0xProfile": "0xa1c584c795533e1b" // The account address where the Profile smart contract lives on Testnet
 })
