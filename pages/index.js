7ffd75cf47152729c686b0e555bdcc520634afb3
Ousikai <mtfallsVR@protonmail.com>
2022-03-03 20:09:26 -0500

    Mutating the Blockchain

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ export default function Home() {
 
   const [user, setUser] = useState({loggedIn: null})
   const [name, setName] = useState('')
+  const [transactionStatus, setTransactionStatus] = useState(null) // NEW
 
   useEffect(() => fcl.currentUser.subscribe(setUser), [])
 
@@ -25,19 +26,18 @@ export default function Home() {
     setName(profile?.name ?? 'No Profile')
   }
 
-  // NEW
   const initAccount = async () => {
     const transactionId = await fcl.mutate({
       cadence: `
         import Profile from 0xProfile
-  
+
         transaction {
           prepare(account: AuthAccount) {
             // Only initialize the account if it hasn't already been initialized
             if (!Profile.check(account.address)) {
               // This creates and stores the profile in the user's account
               account.save(<- Profile.new(), to: Profile.privatePath)
-  
+
               // This creates the public capability that lets applications read the profile's info
               account.link<&Profile.Base{Profile.Public}>(Profile.publicPath, target: Profile.privatePath)
             }
@@ -49,9 +49,33 @@ export default function Home() {
       authorizations: [fcl.authz],
       limit: 50
     })
-  
+
     const transaction = await fcl.tx(transactionId).onceSealed()
     console.log(transaction)
+  }
+
+  // NEW
+  const executeTransaction = async () => {
+    const transactionId = await fcl.mutate({
+      cadence: `
+        import Profile from 0xProfile
+
+        transaction(name: String) {
+          prepare(account: AuthAccount) {
+            account
+              .borrow<&Profile.Base{Profile.Owner}>(from: Profile.privatePath)!
+              .setName(name)
+          }
+        }
+      `,
+      args: (arg, t) => [arg("Flow Developer!", t.String)],
+      payer: fcl.authz,
+      proposer: fcl.authz,
+      authorizations: [fcl.authz],
+      limit: 50
+    })
+
+    fcl.tx(transactionId).subscribe(res => setTransactionStatus(res.status))
   }
 
   const AuthedState = () => {
@@ -59,8 +83,10 @@ export default function Home() {
       <div>
         <div>Address: {user?.addr ?? "No Address"}</div>
         <div>Profile Name: {name ?? "--"}</div>
+        <div>Transaction Status: {transactionStatus ?? "--"}</div> {/* NEW */}
         <button onClick={sendQuery}>Send Query</button>
-        <button onClick={initAccount}>Init Account</button> {/* NEW */}
+        <button onClick={initAccount}>Init Account</button>
+        <button onClick={executeTransaction}>Execute Transaction</button> {/* NEW */}
         <button onClick={fcl.unauthenticate}>Log Out</button>
       </div>
     )
